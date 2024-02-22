81d2db3f0d358fabfdc27ada2fc6217c793a8111
PhongDoan200299 <97325515+PhongDoan200299@users.noreply.github.com>
2024-02-22 11:39:52 +0700

    first commit

---
+++ CS001_HelloWorld/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CS001_HelloWorld
+{
+    class Program
+    {
+        static void MainXXX(string[] args)
+        {
+            Console.WriteLine("Xin chao c#!");
+        }
+    }
+
+    class Abc
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Xin chao c#! ABC");
+
+            Tong(3,2);
+
+        }
+
+        /// <summary>
+        /// Phuong thuc tra ve tong 2 so nguyen
+        /// </summary>
+        /// <param name="a">So nguyen 1</param>
+        /// <param name="b">So nguyen 2</param>
+        /// <returns>Tong a + b</returns>
+        static int Tong(int a,int b)
+        {
+            return(a+b);
+        }
+    }
+}
---
+++ CS002_VariablesConstantsIO/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace CS002_VariablesConstantsIO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Kieu_du_lieu Ten_bien;
+            Ten_bien: 
+                - a..z A..Z
+                - 0 .. 9
+                - _
+                - Không bắt đấu bằng kí tự số         
+            */
+            // String studentName = "PhongDoan";
+            // int studentAge = 20;
+
+            // float c ;
+            // //c = 12.12f;
+            // c = (float)12.12;
+
+            // object d;
+            // d = studentName;
+
+            double so_pi;
+            so_pi = 3.14;
+            double hai_pi;
+            hai_pi = 2*so_pi;
+
+            const double PI = 3.14;
+            const String WED = "THU TU";
+
+            
+            Console.Clear();
+            //Xóa sạch thanh terminal
+            Console.WriteLine(PI);
+            Console.WriteLine(WED);
+
+            
+            Console.Title = "Vi du su dung Console";
+            // Đặt tên cho title 
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            //Đặt màu chữ 
+            Console.BackgroundColor = ConsoleColor.Gray;
+            //Đặt màu background
+
+            Console.WriteLine();
+            Console.WriteLine();
+            // Xuống dòng
+
+            Console.WriteLine("xin chao, chuong trinh nhap xuat du lieu Console");
+
+            Console.ResetColor();
+            //Đặt lại mặc định màu chữ
+
+            String hoVaTen;
+            Console.Write("Ho va ten cua ban:");
+            hoVaTen = Console.ReadLine();
+
+            Console.WriteLine("xin chao {0}", hoVaTen);
+
+            Console.WriteLine("gia tri so pi:");
+            Console.ReadKey();
+            //Nhấn 1 nút bất kì rồi mới thực hiện tác vụ
+
+            float a, b;
+            String sInput;
+
+            Console.WriteLine("Hay nhap vao so a:");
+            sInput = Console.ReadLine();
+            a = float.Parse(sInput);
+
+            Console.WriteLine("Hay nhap vao so b");
+            sInput = Console.ReadLine();
+            b = float.Parse(sInput);
+
+            Console.WriteLine("So a = {0}, b = {1}",a,b);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(so_pi);
+            Console.WriteLine(hai_pi);
+            
+        }
+    }
+}
