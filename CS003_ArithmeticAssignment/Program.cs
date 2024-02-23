1b3eeb559ab8483f36dcdaa790d57311d5141caa
PhongDoan200299 <97325515+PhongDoan200299@users.noreply.github.com>
2024-02-23 17:00:42 +0700

    CS003

---
+++ CS003_ArithmeticAssignment/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace CS003_ArithmeticAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // + - * / %
+            // ()
+            float a = 15;
+            int b = 4;
+
+            float kq = 6 / 2 + 3 * 2;
+
+            Console.WriteLine("a + b = {0}", a+ b);
+            Console.WriteLine("a - b = {0}", a- b);
+            Console.WriteLine("a * b = {0}", a* b);
+            Console.WriteLine("a / b = {0}", a/ b);
+            Console.WriteLine("a % b = {0}", a% b);
+            Console.WriteLine(kq);
+
+            // Gan
+            //  = += -= *= /= %=
+
+            int x = 10;
+            // x ++;
+            // ++x;
+            int z = 2 * x++;
+            // 2 * x;
+            // x = x + 1;
+
+            Console.WriteLine(x);
+            Console.WriteLine(z);
+
+            int y = 10;
+            int m = 2 * ++y;
+            //y = y +1;
+            //2 * y = 22
+            Console.WriteLine(y);
+            Console.WriteLine(m);
+        }
+    }
+}
