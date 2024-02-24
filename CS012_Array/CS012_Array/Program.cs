fbfb8d8688c57fec8b01cf04c6ef1a8b3b21fcbc
PhongDoan200299 <97325515+PhongDoan200299@users.noreply.github.com>
2024-02-24 20:25:24 +0700

    update CS012

--- CS012_Array/CS012_Array/Program.cs
+++ CS012_Array/CS012_Array/Program.cs
@@ -67,19 +67,105 @@ class Program
          */
  //       Double[,] numbers = new double[2,3];
         // 2 hang 3 cot
-        Double[,] numbers = new double[2,3] {{2,3,4.5}, {1,9,8}};
+        // Double[,] numbers = new double[2,3] {{2,3,4.5}, {1,9,8}};
         // Console.WriteLine(numbers[0,2]);
-        int hang = 2;
-        int cot = 3;
-        for (int i = 0; i < hang; i++)
+        // int hang = 2;
+        // int cot = 3;
+        // for (int i = 0; i < hang; i++)
+        // {
+        //     for (int j = 0; j < cot; j++)
+        //     {
+        //         Console.Write( numbers[i, j]);
+        //         Console.Write(" ");
+        //     }
+        //     Console.WriteLine();
+        // }
+        // Console.WriteLine($"Chieu : {numbers.Rank}");
+
+        static void testForEach()
+        // duyet qua cac phan tu mang kieu int, va in ra noi dung phan tu
         {
-            for (int j = 0; j < cot; j++)
+            int[] numbers = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            Array.ForEach<int>(numbers, (int n) => { Console.WriteLine(n); });
+            
+        }
+        testForEach();
+
+        static void testFindAll()
+        //TIm cac so chan (chia het cho 2) trong mang so nguyen 
+        //Roi in ra ket qua
+        {
+            int[] numbers = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            //Dinh nghia Predicate la mot delegate tham so kieu int
+            // tra ve true neu so chia het cho 2
+            Predicate<int> predicate = (int number) =>
             {
-                Console.Write( numbers[i, j]);
-                Console.Write(" ");
+                return number % 2 == 0;
+            };
+            
+            //Tim cac so chan
+            int[] cacsochan = Array.FindAll(numbers, predicate);
+            
+            //In ra ket qua
+            Action<int> printNumber = (int number) => Console.WriteLine(number);
+            Array.ForEach(cacsochan,printNumber);
+        }
+        Console.WriteLine();
+        testFindAll();
+        
+        //Duyet qua cac phan tu mang
+
+        int[] myArray = { 1, 3, 5, 19, 10, 20, 40, 40 };
+        int maxIndex = myArray.Length - 1;
+        for (int i = 0; i <= maxIndex; i++)
+        {
+            Console.WriteLine(myArray[i]);
+        }
+        
+         int[] myArray1 = { 1, 3, 5, 19, 10, 20, 40, 40 };
+        int sum = 0;
+        foreach (var e in myArray1)
+        {
+            sum += e;
+        }
+        Console.WriteLine(sum);
+        
+        //Mang nhieu chieu
+        
+        //Khai bao mang so nguyen hai chieu 3x4 (3 hang 4 cot)
+        int[,] myvar = new int [3, 4];
+        
+        //Truy cap vao phan tu cu the cua mang
+        myvar[0, 0] = 1; // Hang 1, cot 1, gia tri = 1
+        myvar[2, 3] = 4; // Hang 3, cot 4, gia tri = 4
+        
+        Console.WriteLine(myvar);
+
+        int[,] myvar1 = new int [3, 4] { {1,2,3,4 }, {0,3,2,6 }, {8,6,1,0 } };
+        for (int i = 0; i<= 2; i++)
+        {
+            for (int j = 0; j<=3; j++)
+            {
+                Console.Write(myvar1[i,j] + " ");
             }
             Console.WriteLine();
         }
-        Console.WriteLine($"Chieu : {numbers.Rank}");
+
+        int[][] myArray3 = new int[][]
+        {
+            new int[] { 1, 2 },
+            new int[] { 2, 5, 6 },
+            new int[] { 2, 3 },
+            new int[] { 2, 3, 4, 5, 5 }
+        };
+
+        foreach (var arr in myArray3)
+        {
+            foreach (var e in arr)
+            {
+                Console.Write(e + " ");
+            }
+            Console.WriteLine();
+        }
     }
 }
--- CS012_Array/CS012_Array/obj/Debug/net8.0/CS012_Array.AssemblyInfo.cs
+++ CS012_Array/CS012_Array/obj/Debug/net8.0/CS012_Array.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CS012_Array")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d7a710a69a1bbb4e5b2af17c1b6a1b61b21266f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a5f0cfc3b1dc87d727e10ee330614348cc7215d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CS012_Array")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CS012_Array")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
